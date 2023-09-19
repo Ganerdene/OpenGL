2c41b2b40e43b7ad624a2184fefa13035d72a4c4
gan-erdene.mu <gan-erdene.mu@unitel.mn>
2023-09-19 16:42:06 +0800

    aaa

--- OpenGL/Application.cpp
+++ OpenGL/Application.cpp
@@ -4,7 +4,9 @@
 #include <fstream>
 #include <string>
 #include <sstream>
-using namespace std;
+#include "Renderer.h"
+#include "VertexBuffer.h"
+#include "IndexBuffer.h"
 
 struct ShaderProgramSource {
     string vertexSource;
@@ -94,59 +96,72 @@ int main(void)
 
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
-
+    glfwSwapInterval(1);
     if (glewInit() != GLEW_OK) {
         std::cout << "Error" << endl;
     }
 
     std::cout << glGetString(GL_VERSION) << endl;
-
-    float positions[] = {
-        -0.5f, -0.5f,
-         0.5f, -0.5f,
-         0.5f, 0.5f,
-         -0.5f, 0.5f
-    };
-
-    unsigned int indices[] = {
-        0,1,2,
-        2,3,0
-    };
-     
-    unsigned int bufferInt;
-    glGenBuffers(1, &bufferInt);
-    glBindBuffer(GL_ARRAY_BUFFER, bufferInt);
-    glBufferData(GL_ARRAY_BUFFER, 6 * 2 * sizeof(float), positions, GL_STATIC_DRAW);
-
-    glEnableVertexAttribArray(0);
-
-    glVertexAttribPointer(0,2, GL_FLOAT, GL_FALSE, sizeof(float) * 2,  0);
-
-    unsigned int ibo;
-    glGenBuffers(1, &ibo);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int), indices, GL_STATIC_DRAW);
-
-    ShaderProgramSource source = ParseShader("Basic.shader");
-
-    unsigned int shader =  CreateShader(source.vertexSource, source.fragmentSource);
-    glUseProgram(shader);
-
-    /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(window))
     {
-        /* Render here */
-        glClear(GL_COLOR_BUFFER_BIT);
+        float positions[] = {
+            -0.5f, -0.5f,
+             0.5f, -0.5f,
+             0.5f, 0.5f,
+             -0.5f, 0.5f
+        };
 
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
+        unsigned int indices[] = {
+            0,1,2,
+            2,3,0
+        };
+        unsigned int vao;
+        glGenVertexArrays(1, &vao);
+        glBindVertexArray(vao);
 
-        /* Swap front and back buffers */
-        glfwSwapBuffers(window);
+        VertexBuffer vb(positions, 4 * 2 * sizeof(float));
 
-        /* Poll for and process events */
-        glfwPollEvents();
+        glEnableVertexAttribArray(0);
+
+        glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
+
+        IndexBuffer ib(indices, 6);
+
+        ShaderProgramSource source = ParseShader("Basic.shader");
+
+        unsigned int shader = CreateShader(source.vertexSource, source.fragmentSource);
+        glUseProgram(shader);
+
+        int location = glGetUniformLocation(shader, "u_Color");
+        ASSERT(location != -1);
+        //glUniform4f(location, 0.2f, 0.9f, 0.8f, 1.0f);
+
+        float r = 0.0f;
+        float increment = 0.05f;
+
+        /* Loop until the user closes the window */
+        while (!glfwWindowShouldClose(window))
+        {
+            /* Render here */
+            glClear(GL_COLOR_BUFFER_BIT);
+            glUniform4f(location, r, 0.9f, 0.8f, 1.0f);
+            glBindVertexArray(vao);
+            ib.Bind();
+            GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));
+
+            if (r > 1.0f)
+                increment = -0.5f;
+            else if (r < 0.0f)
+                increment = 0.05f;
+
+            r += increment;
+            /* Swap front and back buffers */
+            glfwSwapBuffers(window);
+
+            /* Poll for and process events */
+            glfwPollEvents();
+        }
+        // glDeleteProgram(shader);
     }
-   // glDeleteProgram(shader);
     glfwTerminate();
     return 0;
 }
